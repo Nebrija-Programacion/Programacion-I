a1226425fefda92f627d8101a2640e941e9d5b4c
rocioruizruiz <43112530+rocioruizruiz@users.noreply.github.com>
2018-09-18 21:58:39 +0200

    hello world (#44)
    * mi comentario sobre el archivo
    
    * intento de proyecto con qtcreator
    
    * segundo intento de helloworld
    
    * primer intento de laprimera actividad
    
    * actividad1

---
+++ actividades/Rocio Ruiz Ruiz/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+string nombre{"Rocio"};
+unsigned short int edad{18};
+
+int main()
+{
+    cout << "Hola " <<
+            nombre <<
+            " tienes " <<
+            edad <<
+            " años " <<  endl;
+    return 0;
+}
+
