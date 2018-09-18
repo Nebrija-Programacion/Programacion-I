d124d02f7056e134fc84cb0bdcf1580be20d0150
Alvaro Sanchiz <asanchizp@alumnos.nebrija.es>
2018-09-18 19:11:42 +0200

    Actividad 1 / A.S.

---
+++ actividades/sesion0/AlvaroSanchiz/Actividad1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+string nombre {"Alvaro"};
+unsigned short int edad {18};
+
+
+int main()
+{
+    cout << " Hola soy " << nombre <<" y tengo " << edad << " años " << endl;
+    return 0;
+}
