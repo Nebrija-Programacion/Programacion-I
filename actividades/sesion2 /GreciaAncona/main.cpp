582adeb72da50a08adf8b6906256f4cdac34fe3e
pennnywise98 <43093071+pennnywise98@users.noreply.github.com>
2018-10-06 20:29:44 +0200

    Add files via upload

--- actividades/sesion2 /GreciaAncona/main.cpp
+++ actividades/sesion2 /GreciaAncona/main.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
 
 using namespace std;
-class NumeroEntero{
-private:
-    int num;
+
+class Triangulo {
 public:
-    NumeroEntero( int _num){
-        num = _num;
+    Triangulo( unsigned short int _base , unsigned short int _altura ){
+      base = _base;
+      altura = _altura;
     }
-    int factorial (){
-    if (num <= 0 ) {
-        throw string { " Error : tu unmero no puede ser 0 o menor "} ;
-    }
-    int resultado {1};
-    for ( int cpunter {num}; counter > 0 ; counter - = 1 ) {
-        resultado = resultado * counter;
-    }
-    resultado de devolucion;
-}
-    int getNum(){
-        return num;
+ private:
+    unsigned short int base;
+    unsigned short int altura;
+};
+
+class Rectangulo {
+ private:
+    unsigned short int lado1;
+    unsigned short int lado2;
+ public:
+    Rectangulo( unsigned short int _lado1 , unsigned short int _lado2 ){
+        lado1 = _lado1;
+        lado2 = _lado2;
     }
 };
 
 int main()
 {
-    try {
-        int num ;
-        cout << " Introduce un numero : " << endl;
-        cin >> num ;
-        NumeroEntero tuFactorial (num);
-        cout << " el factorial de " << tuFactorial.getNum() << " es : " << tuFactorial.factorial() << endl;
-    } catch ( string error ) {
-        cout << error << endl;
-    }
+   unsigned int short base , altura , area ;
+   cout << " Dame un valor para la base de tu triangulo : " << endl;
+   cin >> base;
+   cout << " Dame el valor de la altura de tu triangulo : " << endl;
+   cin >> altura;
+   area = ( base * altura )/2;
+   cout << " El area de tu triangulo es :  " << area << endl;
+
+   unsigned short int lado1 , lado2 , areaR , perimetro ;
+   cout << " Dame la medida de un lado del Recangulo : " << endl;
+   cin >> lado1;
+   cout << " Dame el otro lado del rectangulo : " << endl;
+   cin >> lado2;
+   areaR = lado1 * lado2 ;
+   perimetro = 2*lado1 + 2*lado2;
+   cout << " El area de tu rectangulo es : " << areaR << " y el perimetro : " << perimetro << " . " << endl;
     return 0;
 }
