6d45351ff401d155182ff6645f02d3db66e0658b
pennnywise98 <43093071+pennnywise98@users.noreply.github.com>
2018-10-06 20:27:09 +0200

    Add files via upload

---
+++ actividades/sesion2 /GreciaAncona/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i , n , a = 0 ;
+    cout << " Ingrese un numero : " << endl;
+    cin >> n;
+    for ( i{1} ; i < (n + 1 ) ; i ++ ){
+        if (n%1 == 0 ) {
+            a++ ;
+        }
+    };
+    if(a! = 2) {
+        cout << " no es un numero primo " << endl;
+    } else {
+        cout << " Si es primo " << endl;
+    }
+    return 0;
+}
