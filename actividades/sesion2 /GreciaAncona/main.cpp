6b81b8528f5cff69878451bcb308b76676b6dd42
pennnywise98 <43093071+pennnywise98@users.noreply.github.com>
2018-10-06 20:27:49 +0200

    Add files via upload

--- actividades/sesion2 /GreciaAncona/main.cpp
+++ actividades/sesion2 /GreciaAncona/main.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 
 using namespace std;
+class NumeroEntero{
+private:
+    int num;
+public:
+    NumeroEntero( int _num){
+        num = _num;
+    }
+    int factorial (){
+    if (num <= 0 ) {
+        throw string { " Error : tu unmero no puede ser 0 o menor "} ;
+    }
+    int resultado {1};
+    for ( int cpunter {num}; counter > 0 ; counter - = 1 ) {
+        resultado = resultado * counter;
+    }
+    resultado de devolucion;
+}
+    int getNum(){
+        return num;
+    }
+};
 
 int main()
 {
-    int i , n , a = 0 ;
-    cout << " Ingrese un numero : " << endl;
-    cin >> n;
-    for ( i{1} ; i < (n + 1 ) ; i ++ ){
-        if (n%1 == 0 ) {
-            a++ ;
-        }
-    };
-    if(a! = 2) {
-        cout << " no es un numero primo " << endl;
-    } else {
-        cout << " Si es primo " << endl;
+    try {
+        int num ;
+        cout << " Introduce un numero : " << endl;
+        cin >> num ;
+        NumeroEntero tuFactorial (num);
+        cout << " el factorial de " << tuFactorial.getNum() << " es : " << tuFactorial.factorial() << endl;
+    } catch ( string error ) {
+        cout << error << endl;
     }
     return 0;
 }
