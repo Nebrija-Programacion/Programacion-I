e99808d2111b9444b214b407cbc1a785dfe9a9f7
Jorge Gil <jgilr@alumnos.nebrija.es>
2018-10-09 20:17:10 +0200

    sesion 4 Jorge Gil

---
+++ actividades/sesion4/jorgegil/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class NumeroComplejo{
+public:
+    NumeroComplejo(float _r, float _i){
+        real = _r;
+        imaginario = _i;
+    }
+
+    void getNumero(float &r, float &i){
+        r = real;
+        i = imaginario;
+    }
+
+    void suma(float r, float i){
+        real = real + r;
+        imaginario = imaginario + i;
+    }
+
+    void multiplicaPor(float r, float i){
+        real = real * r;
+        imaginario = imaginario * i;
+    }
+
+private:
+    float real;
+    float imaginario;
+};
+
+int main()
+{
+    NumeroComplejo miComplejo{3,4};
+
+    float real;
+    float imaginario;
+
+    miComplejo.suma(2,3);
+
+    miComplejo.getNumero(real, imaginario);
+    cout << "El numero complejo es " << real << " + " << imaginario << " * i" << endl;
+
+    miComplejo.multiplicaPor(2,5);
+    miComplejo.getNumero(real, imaginario);
+    cout << "El numero complejo es " << real << " + " << imaginario << " * i" << endl;
+
+    return 0;
+}
