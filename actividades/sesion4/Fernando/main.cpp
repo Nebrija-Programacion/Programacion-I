d1ee85e6cbd15ae4911e4153bef608d4d6083159
Fernando2706 <43041083+Fernando2706@users.noreply.github.com>
2018-10-10 17:10:57 +0200

    Add files via upload

--- actividades/sesion4/Fernando/main.cpp
+++ actividades/sesion4/Fernando/main.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+class NumeroComplejo
+{
+public:
+    NumeroComplejo(float _real, float _imaginario) {
+        real = _real;
+        imaginario = _imaginario;
+    }
+    void suma(float _num1, float _num2){
+        real+= _num1;
+        imaginario+= _num2;
+    }     //♥
+    void getNumero(float &b, float &a){
+        b = real ;
+        a = imaginario;
+    }
+private:
+    float real;
+    float imaginario;
+};
+int main(){
+
+  float real;
+  float imaginario;
+  cout << "Por favor introduzca la parte real y la imaginaria de su numero complejo: " ;
+  cin >> real >> imaginario;
+  NumeroComplejo miComplejo(real, imaginario);
+
+  miComplejo.suma(2,3);
+
+  miComplejo.getNumero(real, imaginario);
+  cout << "El numero complejo es " << real << " + " << imaginario << " * i" << endl;
+
+
+  return 0;
+}
 
