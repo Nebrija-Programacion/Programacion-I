6fe4cbf1e55dead8f1d2ece7f9de6cbd6f5a4d19
pennnywise98 <43093071+pennnywise98@users.noreply.github.com>
2018-10-14 20:54:52 +0200

    Add files via upload

--- actividades/sesion4/GreciaAncona/Actividad2Sesion4/main.cpp
+++ actividades/sesion4/GreciaAncona/Actividad2Sesion4/main.cpp
@@ -1,53 +1,80 @@
 #include <iostream>
-
+#include <<math.h>
 using namespace std;
-class NumeroComplejo{
 
-   private :
-    float numReal ;
-    float numImag ;
-
+class Vector3D{
+   private:
+    float x;
+    float y;
+    float z;
+    float k;
    public:
-    NumeroComplejo( float _numReal , float _numImag){
-        numReal = _numReal ;
-        numImag = _numImag ;
+    Vector3D(float _x, float _y, float _z , float _k){
+        x = _x ;
+        y = _y ;
+        z = _z ;
+        k = _k;
     }
-    //Funcion de suma de numero complejo y real
-
-    void sum( float r, float i){
-        numImag += i ;
-        numReal += r ;
+    void getCoordinates ( float &a , float &b , float &c){
+        a = x ;
+        b = y ;
+        c = z ;
     }
-    // Funcion de multiplicar el numero complejo y real
-
-    void multiplicarPor ( float r , float i ){
-        numImag *= i ;
-        numReal *= r ;
+    float module( ){
+        return  sqrt ( x^2 + y^2 + z^2);
     }
-    // Funcion por referencia
-
-    void getNumero (float &r , float &i ){
-        r = numReal ;
-        i = numImag ;
+    void scalarMultiplyBy(float num ){
+       x = num * x; // es lo mismo que x*= num
+       y = num * y;
+       z = num * z;
     }
-
+    void add (Vector3D a ){ // a es otro vector con sus componentes
+        x = x + a.x;
+        y = y + a.y;
+        z = z + a.z;
+    }
+    void multiply(Vector3D a){
+        x = x*a.x;
+        y = y*a.y;
+        z = z*a.z;
 };
+    float getX(){return x;}
+    float getY(){return y;}
+    float getz(){return z;}
 
+    void getCoords( float %a , float &b,  float &c){
+        a = x ;
+        b = y ;
+        c = z ;
+
+    }
 int main()
 {
-    NumeroComplejo miComplejo{3,4};
+   float x, y , z, multi ;
+   cout << "  dame x: " << endl;
+   cin >> x;
+   cout << " dame y: " << endl;
+   cin >> y;
+   cout << " dame z: " << endl;
 
-      float real;
-      float imaginario;
+   Vector3D miVector{x,y,z};
+   cout << " el modulo  es: " << miVector.module() << endl;
+   cout << " dame un numero pa multiplicar el vector:  "<< endl;
+   cin >> multi;
+   miVector.scalarMultiplyBy(multi);
+   cout << " el modulo es : " << miVector.module()<< endl;
+   float k;
+   cout << " dame otro numero: " << endl;
+   cin >> k;
+   Vector3D otrovector{x,y,z};
+   miVector.add(otrovector);
+   cout << " X: " << miVector.getX() << endl;
+   cout << " Y: " << miVector.getY() << endl;
+   cout << " Z: " << miVector.getz() << endl;
 
-      miComplejo.sum(2,3);
-
-      miComplejo.getNumero(real, imaginario);
-      cout << "El numero complejo es " << real << " + " << imaginario << " * i" << endl;
-
-      miComplejo.multiplicarPor(2,5);
-      miComplejo.getNumero(real, imaginario);
-      cout << "El numero complejo es " << real << " + " << imaginario << " * i" << endl;
+   miVector.getCoordinates(x, y, z);
+   cout << x << y<< z<< endl;
 
     return 0;
+
 }
