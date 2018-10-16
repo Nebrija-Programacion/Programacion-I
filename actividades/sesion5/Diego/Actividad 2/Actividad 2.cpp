c43582ff47e4f4322fdc27179ed308e593da333b
drodriguezl <43047768+drodriguezl@users.noreply.github.com>
2018-10-16 21:46:32 +0200

    Add files via upload

---
+++ actividades/sesion5/Diego/Actividad 2/Actividad 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+unsigned short int a,b;
+cout <<"introduce un numero : " << endl; cin >> a;
+cout <<"introduce otro numero : " << endl; cin >> b;
+if (a > b ) {cout << "el mayor es : " << a << endl;}else{if (b > a ) {cout << "el mayor es : " << b << endl;} else { cout << "son igueles" << endl;}}
+   }
