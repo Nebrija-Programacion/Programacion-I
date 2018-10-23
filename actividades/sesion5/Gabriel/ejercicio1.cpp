2462eb0a986f5abd547762d0fc8ae1ece76072e8
gsosad <43112434+gsosad@users.noreply.github.com>
2018-10-23 09:37:11 +0200

    Ejercicios sesion 5 de Gabriel (#177)

---
+++ actividades/sesion5/Gabriel/ejercicio1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+bool sonDivisibles(unsigned int a, unsigned int b){
+    if (a % b == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+int main()
+{
+    int a, b;
+    cout << "Haga el favor de introdcuir dos numeros y le dire si el primero es divisible por el segundo.\n";
+    cout << "Introduzca un numero: ";
+    cin >> a;
+    cout << "Introduzca otro numero: ";
+    cin >> b;
+
+    if(sonDivisibles(a, b)== true){
+        cout << "Verdadero";
+    }else{
+        cout << "Falso";
+    }
+    cout << endl;
+
+    return 0;
+}
