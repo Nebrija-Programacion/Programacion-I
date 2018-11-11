87e94d55c3faf92c56673c2e449d2372c0491dd1
Alberto Valero <alberto.valero@bq.com>
2018-11-11 19:57:07 +0100

    ...

--- samuel/hola/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QtCore/QCoreApplication>
-#include <QDebug>
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-    qDebug()  << "Hola Samuel";
-    return a.exec();
-}
