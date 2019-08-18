cf7f5c813d128254cb134de7c2716060a51f5813
Alberto Valero <alberto.valero@bq.com>
2019-08-18 09:13:03 +0200

    restructuracion nuevo curso

--- actividades/sesion0/Angel/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hola Angel!" << endl;
-    return 0;
-}
--- actividades/sesion0/ignaciogonzalez/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Saludos, Ignacio Gonzalez" << endl;
-    return 0;
-}
